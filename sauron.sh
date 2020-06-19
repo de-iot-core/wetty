543170ccfb42d9ac6c8b6ba1f2296c05c415ce6f
Casey Annis <18333704+unklstewy@users.noreply.github.com>
2020-06-19 12:05:46 -0400

    Update sauron.sh
    Added command switches to associate the proper login name with the destination hub on softEther.

--- sauron.sh
+++ sauron.sh
@@ -3,7 +3,17 @@ GW_TARGET="$IP_ADDR"
 JMP_USER="term"
 if [ "$JMP_USER" == "" ] || [ "$GW_TARGET" == ""  ];then
         echo -e "$(basename $0):\n\tInvalid Target Information or No Target Inforamtion Provided:\n\t\t\tExiting in Error State Code: 2"
-	exit 2
+	exit 2& exit 2& logout
 else
-        ssh -J $JMP_USER@172.28.50.11 degw-admin@$GW_TARGET
+	if [ "$HUB" == "gw-residential" ];then
+        	ssh -J $JMP_USER@172.28.50.11 degw-admin@$GW_TARGET
+	elif [ "$HUB" == "gw-default-prov" ];then
+		ssh -J $JMP_USER@172.28.50.11 de-iotcore@$GW_TARGET
+	elif [ "$HUB" == "" ] || [ -z $HUB ];then
+		ssh -J $JMP_USER@172.28.50.11 de-iotcore@$GW_TARGET
+	else
+		echo -e "$(basename $0):\n\tInvalid Target Information or No Target Inforamtion Provided:\n\t\t\tExiting in Error State Code: 2"
+		exit 2& exit 2& logout
+	fi
+		
 fi
