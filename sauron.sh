6da441f35c6096a09933feef84566a57f62ede42
Casey Annis <18333704+unklstewy@users.noreply.github.com>
2020-06-19 12:08:41 -0400

    Update sauron.sh
    Corrected Typo: Inforamtion to Information in 1 occurrences - Casey.

--- sauron.sh
+++ sauron.sh
@@ -2,7 +2,7 @@
 GW_TARGET="$IP_ADDR"
 JMP_USER="term"
 if [ "$JMP_USER" == "" ] || [ "$GW_TARGET" == ""  ];then
-        echo -e "$(basename $0):\n\tInvalid Target Information or No Target Inforamtion Provided:\n\t\t\tExiting in Error State Code: 2"
+        echo -e "$(basename $0):\n\tInvalid Target Information or No Target Information Provided:\n\t\t\tExiting in Error State Code: 2"
 	exit 2& exit 2& logout
 else
 	if [ "$HUB" == "gw-residential" ];then
@@ -12,7 +12,7 @@ else
 	elif [ "$HUB" == "" ] || [ -z $HUB ];then
 		ssh -J $JMP_USER@172.28.50.11 de-iotcore@$GW_TARGET
 	else
-		echo -e "$(basename $0):\n\tInvalid Target Information or No Target Inforamtion Provided:\n\t\t\tExiting in Error State Code: 2"
+		echo -e "$(basename $0):\n\tInvalid Target Information or No Target Information Provided:\n\t\t\tExiting in Error State Code: 2"
 		exit 2& exit 2& logout
 	fi
 		
